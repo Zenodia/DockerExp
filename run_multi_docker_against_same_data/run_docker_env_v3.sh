cd22457e8f348fc5be3d19c2f47f7627a99f096f
Zenodia <Zenodia@users.noreply.github.com>
2019-11-22 17:27:59 +0100

    update to include gpu device allocation

--- run_multi_docker_against_same_data/run_docker_env_v3.sh
+++ run_multi_docker_against_same_data/run_docker_env_v3.sh
@@ -1,1 +1,1 @@
-sudo docker run -e data_path=./data -e load_model=N -e epoch=100 -e batch_size=24 -e model_save=./bz24_weight.h5 --runtime=nvidia -it -p 8181:8181 --rm -v $(pwd):/workspace zenodia/unet_with_vars:v0
+sudo docker run -e data_path=./data -e load_model=N -e epoch=100 -e batch_size=24 -e model_save=./bz24_weight.h5 -e gpu_device_num=2 --runtime=nvidia -it -p 8181:8181 --rm -v $(pwd):/workspace zenodia/unet_with_vars:v0
